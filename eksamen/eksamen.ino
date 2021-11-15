1b291e5be15d35bc78cef770dbf7db17219d8259
anewardeberg <anewardeberg@gmail.com>
2021-11-15 19:12:48 +0100

    logs rolling down

--- eksamen/eksamen.ino
+++ eksamen/eksamen.ino
@@ -3,16 +3,18 @@
 
 // MISC. VARIABLES
 bool userInGame = false;
-int obstacleX = 80;
-int obstacleY = 30;
+int obstacleX = 60;
+int obstacleY = -30;
 int carX = 115;
 int carMaxX = 169;
 int carMinX = 61;
-int speed = 3;
+int gameSpeed = 1;
 int score = 0;
 int lazersAvailable = 5;
 int speedUpScore = 0;
 int highScore = 0;
+
+//int deltatime SPØR MATS OM DETTE NÅR DEN TID KOMMER <3
 
 
 
@@ -21,6 +23,7 @@ int highScore = 0;
 #include <SPI.h>
 
 #define BLACK 0x0000
+#define BROWN 0x7A40
 #define GREEN 0x07E0
 #define GRAY 0x9492
 #define BLUE 0x063F
@@ -102,8 +105,11 @@ void loop() {
   //  tft.print("to start game");
   showHomeScreen();
   startGame(2);
+
   delay(200);
   while (userInGame) {
+    obstacleY = obstacleY + gameSpeed;
+    drawPillars(obstacleX, obstacleY);
     buttonState3 = digitalRead(buttonPin3);
     buttonState2 = digitalRead(buttonPin2);
     buttonState4 = digitalRead(buttonPin4);
@@ -113,6 +119,19 @@ void loop() {
     // map it to the range of the analog out:
     outputValueX = map(sensorValueX, 0, 1023, 0, 255);
     //    outputValueY = map(sensorValueY, 0, 1023, 0, 255);
+    //     if((xP<=85) && (xP>=5) && (yB<=yP-2)){ // upper pillar
+    //      gameOver();
+    //    }
+    //    if((xP<=85) && (xP>=5) && (yB>=yP+60)){ // lower pillar
+    //      gameOver();
+    //    }
+    if (obstacleY >= 150) {
+      obstacleY = -30; // Resets xP to 319
+      obstacleX = rand() % 80 + 60; // Random number for the pillars height
+      score++; // Increase score by one
+      updateGameStats();
+    }
+
     if (buttonState3 == HIGH && lazersAvailable > 0) {
       activateLazer(carX);
       updateGameStats();
@@ -153,17 +172,17 @@ void loop() {
 
 void showHomeScreen() {
   tft.fillCircle(20, 70, 10, GRAY);
-  tft.fillCircle(40, 50, 10, PINK);
+  tft.fillCircle(40, 50, 10, GRAY);
   tft.fillCircle(60, 70, 10, GREEN);
   tft.fillCircle(40, 90, 10, GRAY);
 
-  tft.setTextSize(1.5);
-  tft.fillCircle(90, 50, 8, GREEN);
-  tft.setCursor(105, 47);
+  tft.setTextSize(2);
+  //  tft.fillCircle(90, 50, 8, GREEN);
+  //  tft.setCursor(105, 47);
+  //  tft.print("Start game");
+  //  tft.fillCircle(90, 70, 8, PINK);
+  tft.setCursor(90, 63);
   tft.print("Start game");
-  tft.fillCircle(90, 70, 8, PINK);
-  tft.setCursor(105, 67);
-  tft.print("Car color");
 }
 
 void startGame(int buttonPin) {
@@ -187,6 +206,7 @@ void chooseCarColor() {
   bool userHasChosenColor = false;
   int i = 0;
   tft.fillScreen(BLACK);
+  tft.setCursor(25, 40);
   tft.print("CHOOSE CAR COLOR");
   while (!userHasChosenColor) {
     buttonState1 = digitalRead(buttonPin1);
@@ -194,46 +214,44 @@ void chooseCarColor() {
     tft.fillRect(73, 80, 34, 34, carColorArray[1]);
     tft.fillRect(129, 80, 34, 34, carColorArray[2]);
     tft.fillRect(185, 80, 34, 34, carColorArray[3]);
-    Serial.println(outputValueX);
-    
+    //    Serial.println(outputValueX);
+    //
     sensorValueX = analogRead(analogInPinX);
     outputValueX = map(sensorValueX, 0, 1023, 0, 255);
-    tft.drawRect(14, 77, 40, 40, GREEN);
-    switch (outputValueX) {
-      case 160 ... 255:
-        i++;
-        if (i > 3) {
-          i = 3;
-        }
-      case 0 ... 80:
-        i--;
-        if (i < 0) {
-          i = 0;
-        }
-      default:
+    Serial.print(outputValueX);
+    if (outputValueX >= 160) {
+      i++;
+      if (i > 3) {
+        i = 3;
+      }
+      delay(200);
+    } else if (outputValueX <= 80) {
+      i--;
+      if (i < 0) {
         i = 0;
+      }
+      delay(200);
     }
 
-    while (i = 0) {
+    Serial.print("i: ");
+    Serial.println(i);
+
+    if (i = 0) {
       tft.drawRect(14, 77, 40, 40, GREEN);
       tft.drawRect(70, 77, 40, 40, BLACK);
       tft.drawRect(126, 77, 40, 40, BLACK);
       tft.drawRect(182, 77, 40, 40, BLACK);
-    }
-
-    while (i = 1) {
+    } else if (i = 1) {
       tft.drawRect(14, 77, 40, 40, BLACK);
       tft.drawRect(70, 77, 40, 40, GREEN);
       tft.drawRect(126, 77, 40, 40, BLACK);
       tft.drawRect(182, 77, 40, 40, BLACK);
-    }
-    while (i = 2) {
+    } else if (i = 2) {
       tft.drawRect(14, 77, 40, 40, BLACK);
       tft.drawRect(70, 77, 40, 40, BLACK);
       tft.drawRect(126, 77, 40, 40, GREEN);
       tft.drawRect(182, 77, 40, 40, BLACK);
-    }
-    while (i = 3) {
+    } else if (i = 3) {
       tft.drawRect(14, 77, 40, 40, BLACK);
       tft.drawRect(70, 77, 40, 40, BLACK);
       tft.drawRect(126, 77, 40, 40, BLACK);
@@ -326,26 +344,23 @@ void updateGameStats() {
   showGameStats();
 }
 
-void drawPillars(int x, int y) {
-  if (x >= 270) {
-    tft.fillRect(318, 0, x, y - 1, PINK);
-    tft.drawRect(319, 0, x - 1, y, BLACK);
-    tft.fillRect(318, y + 81, x, 203, GRAY);
-    tft.drawRect(319, y + 80, x - 1, 204, BLACK);
-  }
-  else if ( x <= 268) {
-    // Draws blue rectangle right of the pillar
-    tft.fillRect(x + 51, 0, x + 60, y, BLUE);
-    // Draws the pillar
-    tft.fillRect(x + 49, 1, x + 1, y - 1, GRAY);
+void beep() {
+  // insert tut-funksjon here
+  //  (takk mats)
+}
 
-    tft.drawRect(x + 50, 0, x, y, BLACK);
-    // Draws the blue rectangle left of the pillar
-    tft.fillRect(x - 1, 0, x - 3, y, BLUE);
-    // The bottom pillar
-    tft.fillRect(x + 51, y + 80, x + 60, 204, PINK);
-    tft.fillRect(x + 49, y + 81, x + 1, 203, GRAY);
-    tft.drawRect(x + 50, y + 80, x, 204, BLACK);
-    tft.fillRect(x - 1, y + 80, x - 3, 204, BLUE);
+void drawPillars(int x, int y) {
+  //  if (x >= 270) {
+  tft.fillRect(x, y, 40, 15, BROWN);
+//  tft.fillRect(x + 90, y, 40, 15, BROWN);
+  tft.fillRect(x, y - 15, 40, 15, ASPHALT_GRAY);
+//  tft.fillRect(x + 90, y - 15, 40, 15, ASPHALT_GRAY);
+  if(x <= 60) {
+    
   }
+}
+
+void gameOver() {
+  tft.fillScreen(BLACK);
+  tft.print("GAME OVER");
 }
